2de8efd8f4a61dec51b1278d0501b3fbd8df4c5f
Jorge Cuza <jorge.cuza92@gmail.com>
2021-03-03 20:30:31 -0600

    CSS styling to weather application

--- day3/weather-app/js/weather.js
+++ day3/weather-app/js/weather.js
@@ -2,34 +2,31 @@
 const cityTextBox = document.getElementById('cityTextBox')
 const btnCity = document.getElementById('btnCity')
 const infoUL = document.getElementById('infoUL')
-let city = cityTextBox.value 
+let city = cityTextBox.value
 
-// Button function 
 btnCity.addEventListener('click', function () {
 
-  const city = cityTextBox.value
   getAllInfo()
 
 })
 
-// FUNCTION FOR CITY SEARCH
 function getAllInfo() {
-  let city = cityTextBox.value 
+  let city = cityTextBox.value
   let weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ae9b20fe49863588f2d13125d0bfde0d&units=imperial`
   fetch(weatherURL)
     .then((response) => {
       return response.json()
     }).then((info) => {
       console.log(info)
-      let infoItems = 
+      let infoItems =
                     ` <h1>${city}</h1>
-                      <li>Temperature: ${info.main.temp}°F</li>
-                      <li>Maximum Temperature: ${info.main.temp_max}°F</li>
-                      <li>Minimun Temperature: ${info.main.temp_min}°F</li>
-                      <li>Pressure: ${info.main.pressure} mbar</li>
+                      <li id="textItem">Temperature: ${info.main.temp}°F</li>
+                      <li id="textItem">Maximum Temperature: ${info.main.temp_max}°F</li>
+                      <li id="textItem">Minimun Temperature: ${info.main.temp_min}°F</li>
+                      <li id="textItem">Pressure: ${info.main.pressure} mbar</li>
                     `
       infoUL.innerHTML = infoItems
     })
-      
-  }
+
+}
 
